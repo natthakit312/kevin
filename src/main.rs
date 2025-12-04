1fdad4ab55a4f5204436b304f83e8980c5238ef0
natthakit312 <natthakit.kaewbun@gmail.com>
2025-12-04 17:20:00 +0700

    first commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use server::config::config_loader;
+use tracing::{error, info};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
+
+    let _dotenvy_env = match config_loader::load() {
+        Ok(env) => env,
+        Err(e) => {
+            error!("Failed to load ENV: {}", e);
+            std::process::exit(1);
+        }
+    };
+
+    info!("foremost server is starting...");
+}
